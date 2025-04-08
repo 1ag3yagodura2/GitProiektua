05a932f291de759adc9e911d45b672de22e4e171
Stefan Gheorghe Veciunca <1ag3.stefveci@tolosaldealh.eus>
2025-04-08 08:50:31 +0200

    Webgunea sortuta

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("actionBtn").addEventListener("click", () => {
+    const output = document.getElementById("output");
+    output.textContent = "Eskerrik asko bisitagatik! :)";
+  });
+  
